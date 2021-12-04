065c1c836f9f1354b44d4300e52f150d07db23ca
MrMinimal64 <github@michelfe.it>
2021-12-04 19:47:18 +0100

    Update __init__.py

--- multivar_horner/__init__.py
+++ multivar_horner/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
+from multivar_horner.classes.abstract_poly import load_pickle
 from multivar_horner.classes.horner_poly import HornerMultivarPolynomial, HornerMultivarPolynomialOpt
 from multivar_horner.classes.regular_poly import MultivarPolynomial
 
-__all__ = ["HornerMultivarPolynomial", "MultivarPolynomial", "HornerMultivarPolynomialOpt"]
+__all__ = ["HornerMultivarPolynomial", "MultivarPolynomial", "HornerMultivarPolynomialOpt", "load_pickle"]
