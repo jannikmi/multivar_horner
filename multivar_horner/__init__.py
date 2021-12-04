f72b9bce0042af14f7f41649a6b5c0d617a447bc
MrMinimal64 <github@michelfe.it>
2021-12-04 18:41:14 +0100

    Update __init__.py

--- multivar_horner/__init__.py
+++ multivar_horner/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding:utf-8 -*-
-from multivar_horner.classes.horner_poly import HornerMultivarPolynomial
+from multivar_horner.classes.horner_poly import HornerMultivarPolynomial, HornerMultivarPolynomialOpt
 from multivar_horner.classes.regular_poly import MultivarPolynomial
 
-# https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
-# determines which objects will be imported with "import *"
-__all__ = ["HornerMultivarPolynomial", "MultivarPolynomial"]
+__all__ = ["HornerMultivarPolynomial", "MultivarPolynomial", "HornerMultivarPolynomialOpt"]
